2bfd9b288ed8f56e635e7189be2c352b670d1b89
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-25 15:51:27 +0300

    Assignment completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const chai = chai();
+const spies = ("chai-spies");
+chai.use(spies);
+function receivesAFunction(callback) {
+     callback();
+    
+}
+function returnsANamedFunction() {
+    return function fn(){
+        "returns a function"
+}
+}
+
+function returnsAnAnonymousFunction(){
+    return function() {
+
+    }
+}
